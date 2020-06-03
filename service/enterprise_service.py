80031c14a4e651422bef49ce11947d049d62d8ad
zhantanfeng <1148705128@qq.com>
2020-06-03 09:58:39 +0800

    windows

--- service/enterprise_service.py
+++ service/enterprise_service.py
@@ -3,6 +3,7 @@
 """
 import dao.enterprise_dao as enterprise_dao
 
+
 def get_en_info_by_name(name):
     """
     根据企业名获取企业列表
@@ -11,6 +12,7 @@ def get_en_info_by_name(name):
     """
     result = enterprise_dao.get_en_info_by_name_2(name)
     return result
+
 
 def get_en_info_by_scope(scope):
     """
